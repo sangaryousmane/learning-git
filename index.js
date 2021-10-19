89220ff4b726b4ba1372293e96faafa805e0be7d
Ousmane <sangaryousmane@gmail.com>
2021-10-19 14:07:16 +0800

    The updated async function

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello js");
+
+async function doubleAndAdd(a,b){
+    a = await doubleAfter1Second(a);
+    b = await doubleAfter1Second(b);
+    
+    return a + b;
+    }
+    
+    doubleAndAdd(1,2).then(console.log);
+    
+    function doubleAfter1Second(param){
+        return new Promise(resolve =>{
+            setTimeout(resolve(param * 2), 1000);
+        });
+    }
